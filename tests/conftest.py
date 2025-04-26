5e87945d427dea6b258025e4996b8b74e4d70c58
Diyar Oktay <diyar.oktay@outlook.com>
2025-04-26 19:03:51 +0200

    format

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ.setdefault('OPENROUTER_API_KEY', "")
-os.environ.setdefault('OPENROUTER_API_URL', "")
-os.environ.setdefault('DISCORD_TOKEN', "")
-os.environ.setdefault('DISCORD_GUILD_ID', "-1")
+os.environ.setdefault("OPENROUTER_API_KEY", "")
+os.environ.setdefault("OPENROUTER_API_URL", "")
+os.environ.setdefault("DISCORD_TOKEN", "")
+os.environ.setdefault("DISCORD_GUILD_ID", "-1")
