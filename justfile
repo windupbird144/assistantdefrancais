3a6d31ec9ee20eb00c95a35783cfcab127139453
Diyar Oktay <diyar.oktay@outlook.com>
2025-04-26 13:30:19 +0200

    Initial commit

---
+++ justfile
@@ -0,0 +1,4 @@
+test:
+    # Invoking `python -m pytest` adds the current directory
+    # to sys.path, which lets us import from the project root
+    uv run python -m pytest
---
+++ tests/test_bot.py
@@ -0,0 +1,13 @@
+from unittest.mock import AsyncMock
+from bot import definir  # The decorated command
+import pytest
+
+@pytest.mark.asyncio
+async def test_definir_command():
+    mock_interaction = AsyncMock()
+    mock_interaction.response = AsyncMock()
+    
+    await definir._do_call(mock_interaction, {"mot" : "essor"})
+    
+    mock_interaction.response.send_message.assert_called_once()
+    assert "essor" in mock_interaction.response.send_message.call_args[0][0]
